dcd67def4caa0377582e3c9f773a3e1b616ba11d
janniknu <jannik.nuesslein@gmx.de>
2024-12-26 11:47:00 +0100

    added order entitiy

--- backend/orderservice/src/main/java/de/thi/orderservice/jpa/Ordering.java
+++ backend/orderservice/src/main/java/de/thi/orderservice/jpa/Ordering.java
@@ -1,19 +1,96 @@
 package de.thi.orderservice.jpa;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@Table(name = "orders")
 public class Ordering extends PanacheEntity {
 
-    private String field;
+    @Column(name = "customer_id", nullable = false)
+    private Long customerId;
 
-    public String getField() {
-        return field;
+    @ManyToMany
+    @JoinTable(
+            name = "order_meals",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "meal_id")
+    )
+    private List<Meal> meals;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_drinks",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "drink_id")
+    )
+    private List<Drink> drinks;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(name = "processor_id", nullable = false)
+    private Long processorId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private StatusEnum status;
+
+    public enum StatusEnum {
+        PROCESSING,
+        COMPLETED,
+        FAILED
     }
 
-    public void setField(String field) {
-        this.field = field;
+
+    public Long getCustomerId() {
+        return customerId;
     }
 
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(List<Drink> drinks) {
+        this.drinks = drinks;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Long getProcessorId() {
+        return processorId;
+    }
+
+    public void setProcessorId(Long processorId) {
+        this.processorId = processorId;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
 }
