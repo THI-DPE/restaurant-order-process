1fe50312e06faa533ea4a03dcaea542aee09814d
Marvin <kern.marvin@outlook.de>
2025-01-09 23:18:28 +0100

    Added MenuService products endpoint

--- backend/orderservice/src/main/java/de/thi/orderservice/service/external/MenuService.java
+++ backend/orderservice/src/main/java/de/thi/orderservice/service/external/MenuService.java
@@ -1,6 +1,8 @@
 package de.thi.orderservice.service.external;
 
 import de.thi.orderservice.model.Category;
+import de.thi.orderservice.model.Product;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -18,5 +20,9 @@ public interface MenuService {
     @GET
     @Path("/categories/{categoryId}")
     Category getCategoryById(@PathParam("categoryId") Long id);
+
+    @GET
+    @Path("/products/{productId}")
+    Product getProductById(@PathParam("productId") Long id);
 
 }
