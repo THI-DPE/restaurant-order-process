50f6130d2e2072fd0c075eb5b3ee95a425de8297
Marvin <kern.marvin@outlook.de>
2025-01-11 17:45:52 +0100

    Added new /products/total endpoint

--- backend/menuservice/src/main/java/de/thi/menuservice/rest/ProductController.java
+++ backend/menuservice/src/main/java/de/thi/menuservice/rest/ProductController.java
@@ -1,15 +1,15 @@
 package de.thi.menuservice.rest;
 
 import de.thi.menuservice.jpa.Product;
-import de.thi.menuservice.service.CategoryService;
 import de.thi.menuservice.service.ProductService;
-import de.thi.menuservice.service.ProductServiceImpl;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Path("/products")
 public class ProductController {
@@ -31,5 +31,22 @@ public class ProductController {
         return Response.status(Response.Status.NOT_FOUND).build();
     }
 
+    @GET
+    @Path("/total")
+    public Response getProductDetailsAndTotalPrice(@QueryParam("id") List<Long> productIds) {
+        Map<String, Object> response = new HashMap<>();
+        List<Product> products = new ArrayList<>();
+        double totalPrice = 0;
+        for (Long productId : productIds) {
+            Product product = productService.findById(productId);
+            if (product != null) {
+                totalPrice += product.getPrice();
+                products.add(product);
+            }
+        }
+        response.put("totalPrice", totalPrice);
+        response.put("products", products);
+        return Response.ok(response).build();
+    }
 
 }
